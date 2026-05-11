511bfbc2a2b92d707e459e280e338ee196abd8d0
Rajaprabu <aix.rajaprabu@gmail.com>
2026-05-11 16:17:29 +0530

    RBAC-removed

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -61,17 +61,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 
   # =====================================
-  # Azure AD + Azure RBAC
-  # =====================================
-
-  azure_active_directory_role_based_access_control {
-
-    azure_rbac_enabled = true
-
-    tenant_id = data.azurerm_client_config.current.tenant_id
-  }
-
-  # =====================================
   # Network Profile
   # =====================================
 
