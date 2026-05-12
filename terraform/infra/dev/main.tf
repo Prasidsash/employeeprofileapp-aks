bbc2c36d5170dcf55aa63ec606e2575c63b5ae8a
Rajaprabu <aix.rajaprabu@gmail.com>
2026-05-12 15:34:30 +0530

    Fix enterprise Terraform infra pipeline

--- terraform/infra/dev/main.tf
+++ terraform/infra/dev/main.tf
@@ -72,3 +72,63 @@ module "aks" {
 
   dns_service_ip = var.dns_service_ip
 }
+
+module "namespace" {
+
+  source = "../../modules/employee_namespace"
+
+  count = var.enable_namespace ? 1 : 0
+
+  namespace_name = var.namespace_name
+
+  namespace_labels = var.namespace_labels
+}
+
+module "governance" {
+
+  source = "../../modules/employee_governance"
+
+  count = var.enable_governance ? 1 : 0
+
+  namespace_name = var.namespace_name
+
+  quota_limits = var.quota_limits
+
+  limit_max = var.limit_max
+
+  limit_min = var.limit_min
+
+  limit_default = var.limit_default
+
+  limit_default_request = var.limit_default_request
+}
+
+module "rbac" {
+
+  source = "../../modules/employee_rbac"
+
+  count = var.enable_rbac ? 1 : 0
+
+  namespace_name = var.namespace_name
+
+  service_account_name = var.service_account_name
+
+  role_name = var.role_name
+
+  allowed_resources = var.allowed_resources
+
+  allowed_verbs = var.allowed_verbs
+}
+
+module "secret" {
+
+  source = "../../modules/employee_secret"
+
+  count = var.enable_secret ? 1 : 0
+
+  namespace_name = var.namespace_name
+
+  secret_name = var.secret_name
+
+  secret_data = var.secret_data
+}
