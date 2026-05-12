190f1eddeffb90c1cbedd91b9b82cb8f72eecefd
Rajaprabu <aix.rajaprabu@gmail.com>
2026-05-12 18:30:54 +0530

    12/05/2026 @ 630pm

--- src/Employeeprofileapp/Program.cs
+++ src/Employeeprofileapp/Program.cs
@@ -98,7 +98,7 @@ string Page(string title, string emoji, string message)
 
 app.MapGet("/", async context =>
 {
-    await context.Response.WriteAsync(Page("Home", "🏠", "Welcome to Hawaria"));
+    await context.Response.WriteAsync(Page("Home", "🏠", "Welcome to TVK Vijay"));
 });
 
 app.MapGet("/employee", async context =>
