59f35d717b11cbda0d498a633468d21616e699db
Rajaprabu <aix.rajaprabu@gmail.com>
2026-05-20 12:54:25 +0530

    20/05/2026 @ 12:54 PM

--- src/Employeeprofileapp/Program.cs
+++ src/Employeeprofileapp/Program.cs
@@ -98,7 +98,7 @@ string Page(string title, string emoji, string message)
 
 app.MapGet("/", async context =>
 {
-    await context.Response.WriteAsync(Page("Home", "🏠", "Welcome to 20/05/2026 @ 10:23 AM"));
+    await context.Response.WriteAsync(Page("Home", "🏠", "Welcome to 20/05/2026 @ 12:53 PM"));
 });
 
 app.MapGet("/employee", async context =>
