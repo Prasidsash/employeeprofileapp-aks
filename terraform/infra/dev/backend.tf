14b676ae4e641a3132469910d158e19eb4f9c6e8
Rajaprabu <aix.rajaprabu@gmail.com>
2026-05-25 17:32:08 +0530

    25/05/2026 @ 05:32 PM

--- terraform/infra/dev/backend.tf
+++ terraform/infra/dev/backend.tf
@@ -8,7 +8,7 @@ terraform {
 
     resource_group_name  = "employeeprofileapp-tfstate-rg"
 
-    storage_account_name = "empproftfstate2026"
+    storage_account_name = "empproftfstate2027"
 
     container_name       = "tfstate"
 
